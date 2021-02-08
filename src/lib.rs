e936958d2aeca99b2f3b54806b4ba46a24aaf189
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2021-02-08 19:36:26 +0200

    expose http client on top of methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
-}
+#[cfg(feature = "default")]
+pub extern crate tokio;
+
+#[cfg(feature = "default")]
+pub use tokio::prelude::*;
+#[cfg(feature = "default")]
+pub use tokio::runtime::Runtime;
+
+#[cfg(feature = "default")]
+use futures::prelude::*;
+use hyper::{client::HttpConnector, Client};
+use hyper_tls::HttpsConnector;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::rc::Rc;
+
+/// Reference counted Hyper client with custom https connector
+pub type HttpsClient = Rc<Client<HttpsConnector<HttpConnector>, hyper::Body>>;
